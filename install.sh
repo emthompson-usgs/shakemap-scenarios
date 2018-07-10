d8ff300894588472fce8b7cdcd9c30e346c2cf2a
emthompson@usgs.gov <emthompson@usgs.gov>
2018-07-10 08:39:00 -0600

    Fixed install script

--- install.sh
+++ install.sh
@@ -1,97 +1,152 @@
 #!/bin/bash
-echo $PATH
-
-VENV=scenarios
-PYVER=3.5
-
-# Is conda installed?
-conda=$(which conda)
-if [ ! "$conda" ] ; then
-    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh \
-        -O miniconda.sh;
-    bash miniconda.sh -f -b -p $HOME/miniconda
-    export PATH="$HOME/miniconda/bin:$PATH"
-fi
-
-conda update -q -y conda
-conda config --prepend channels conda-forge
-conda config --append channels digitalglobe # for rasterio v 1.0a9
-conda config --append channels ioos # for rasterio v 1.0a2
 
 unamestr=`uname`
-if [[ "$unamestr" == 'Linux' ]]; then
-    DEPARRAY=(numpy=1.11 scipy=0.19.1 matplotlib=2.0.2 rasterio=1.0a2 \
-        pandas=0.20.3 h5py=2.7.0 gdal=2.1.4 pytest=3.2.0 pytest-cov=2.5.1 \
-        cartopy=0.15.1 fiona=1.7.8 numexpr=2.6.2 configobj=5.0.6 \
-        decorator=4.1.2 versioneer==0.18 git=2.13.3)
-elif [[ "$unamestr" == 'FreeBSD' ]] || [[ "$unamestr" == 'Darwin' ]]; then
-    DEPARRAY=(numpy=1.13.1 scipy=0.19.1 matplotlib=2.0.2 rasterio=1.0a9 \
-        pandas=0.20.3 h5py=2.7.0 gdal=2.1.4 pytest=3.2.0 pytest-cov=2.5.1 \
-        cartopy=0.15.1 fiona=1.7.8 numexpr=2.6.2 configobj=5.0.6 \
-        decorator=4.1.2 versioneer==0.18)
-fi
-# NOTE: I added git to the linux deps list beacuse the ancient version on
-#       Scientific Linux (on Atlas) causes errors.
-
-
-# Is the Travis flag set?
-travis=0
-while getopts t FLAG; do
-  case $FLAG in
-    t)
-      travis=1
-      ;;
-  esac
-done
-if [ $travis == 0 ] ; then
-    DEPARRAY+=(ipython=6.1.0 spyder=3.2.1 jupyter=1.0.0 seaborn=0.8.0 \
-        sphinx=1.6.3)
+if [ "$unamestr" == 'Linux' ]; then
+    prof=~/.bashrc
+    mini_conda_url=https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+    matplotlibdir=~/.config/matplotlib
+elif [ "$unamestr" == 'FreeBSD' ] || [ "$unamestr" == 'Darwin' ]; then
+    prof=~/.bash_profile
+    mini_conda_url=https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
+    matplotlibdir=~/.matplotlib
+else
+    echo "Unsupported environment. Exiting."
+    exit
 fi
 
-# turn off whatever other virtual environment user might be in
-source deactivate
+source $prof
 
-# remove any previous virtual environments called pager
-CWD=`pwd`
-cd $HOME;
-conda remove --name $VENV --all -y
-cd $CWD
+# echo "Path:"
+# echo $PATH
+
+VENV=scenarios
+
+
+# this is an experiment to see if we can get away without the platform specific
+# environment files.
+reset=1
+
+# create a matplotlibrc file with the non-interactive backend "Agg" in it.
+if [ ! -d "$matplotlibdir" ]; then
+    mkdir -p $matplotlibdir
+    # if mkdir fails, bow out gracefully
+    if [ $? -ne 0 ];then
+        echo "Failed to create matplotlib configuration file. Exiting."
+        exit 1
+    fi
+fi
+matplotlibrc=$matplotlibdir/matplotlibrc
+if [ ! -e "$matplotlibrc" ]; then
+    echo "backend : Agg" > "$matplotlibrc"
+    echo "NOTE: A non-interactive matplotlib backend (Agg) has been set for this user."
+elif grep -Fxq "backend : Agg" $matplotlibrc ; then
+    :
+elif [ ! grep -Fxq "backend" $matplotlibrc ]; then
+    echo "backend : Agg" >> $matplotlibrc
+    echo "NOTE: A non-interactive matplotlib backend (Agg) has been set for this user."
+else
+    sed -i '' 's/backend.*/backend : Agg/' $matplotlibrc
+    echo "###############"
+    echo "NOTE: $matplotlibrc has been changed to set 'backend : Agg'"
+    echo "###############"
+fi
+
+
+# Is conda installed?
+conda --version
+if [ $? -ne 0 ]; then
+    echo "No conda detected, installing miniconda..."
+
+    command -v curl >/dev/null 2>&1 || { echo >&2 "Script requires curl but it's not installed. Aborting."; exit 1; }
+
+    curl $mini_conda_url -o miniconda.sh;
+
+    # if curl fails, bow out gracefully
+    if [ $? -ne 0 ];then
+        echo "Failed to create download miniconda installer shell script. Exiting."
+        exit 1
+    fi
+    
+    echo "Install directory: $HOME/miniconda"
+
+    bash miniconda.sh -f -b -p $HOME/miniconda
+
+    # if miniconda.sh fails, bow out gracefully
+    if [ $? -ne 0 ];then
+        echo "Failed to run miniconda installer shell script. Exiting."
+        exit 1
+    fi
+    
+    . $HOME/miniconda/etc/profile.d/conda.sh
+else
+    echo "conda detected, installing $VENV environment..."
+fi
+
+# echo "PATH:"
+# echo $PATH
+# echo ""
+
+
+# Choose an environment file based on platform
+# only add this line if it does not already exist
+grep "/etc/profile.d/conda.sh" $prof
+if [ $? -ne 0 ]; then
+    echo ". $_CONDA_ROOT/etc/profile.d/conda.sh" >> $prof
+fi
+
+env_file=environment.yml
+
+
+# Start in conda base environment
+echo "Activate base virtual environment"
+conda activate base
 
 # Create a conda virtual environment
-echo "Creating the $VENV virtual environment"
-echo "with the following dependencies:"
-echo ${DEPARRAY[*]}
-conda create --name $VENV -y python=$PYVER ${DEPARRAY[*]}
+echo "Creating the $VENV virtual environment:"
+conda env create -f $env_file --force
+
+# Bail out at this point if the conda create command fails.
+# Clean up zip files we've downloaded
+if [ $? -ne 0 ]; then
+    echo "Failed to create conda environment.  Resolve any conflicts, then try again."
+    exit
+fi
+
 
 # Activate the new environment
 echo "Activating the $VENV virtual environment"
-source activate $VENV
+conda activate $VENV
 
-# OpenQuake v2.5.0
-echo "Downloading OpenQuake v2.5.0..."
-curl --max-time 60 --retry 3 -L \
-    https://github.com/gem/oq-engine/archive/v2.5.0.zip -o openquake.zip
-pip -q install --no-deps openquake.zip
-rm openquake.zip
+# if conda activate fails, bow out gracefully
+if [ $? -ne 0 ];then
+    echo "Failed to activate ${VENV} conda environment. Exiting."
+    exit 1
+fi
 
-# MapIO and impact-utils
-echo "Installing MapIO..."
-pip -q install https://github.com/usgs/MapIO/archive/master.zip
-echo "Installing impact-utils..."
-pip -q install \
-    https://github.com/usgs/earthquake-impact-utils/archive/master.zip
+# upgrade pip, mostly so pip doesn't complain about not being new...
+pip install --upgrade pip
 
-# Shakelib
-echo "Installing shakelib..."
-pip -q install https://github.com/usgs/shakelib/archive/master.zip
+# if pip upgrade fails, complain but try to keep going
+if [ $? -ne 0 ];then
+    echo "Failed to upgrade pip, trying to continue..."
+    exit 1
+fi
 
-# Shakemap
+
+# shakemap 4
 echo "Installing shakemap..."
-pip -q install https://github.com/usgs/shakemap/archive/master.zip
+pip install --no-deps https://github.com/usgs/shakemap/archive/master.zip
 
-# Install shakemap-scearios
-echo "Installing shakemap..."
-pip install -e .
 
-# tell the user they have to activate this environment
-echo "Type 'source activate scenarios' to use this new virtual environment."
+# This package
+echo "Installing ${VENV}..."
+pip install --no-deps -e .
+
+# if pip install fails, bow out gracefully
+if [ $? -ne 0 ];then
+    echo "Failed to pip install this package. Exiting."
+    exit 1
+fi
+
+# Tell the user they have to activate this environment
+echo "Type 'conda activate $VENV' to use this new virtual environment."
